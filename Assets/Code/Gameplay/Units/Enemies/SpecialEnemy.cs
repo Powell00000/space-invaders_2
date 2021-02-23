d5d5f07a16a8cf92c49d7cd22bf9ba28451a5426
Paweł Miłaszewicz <pawelm@kool2play.com>
2021-02-23 18:06:41 +0100

    base for special enemy

---
+++ Assets/Code/Gameplay/Units/Enemies/SpecialEnemy.cs
@@ -0,0 +1,35 @@
+﻿using Game.Gameplay;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Code.Gameplay.Units.Enemies
+{
+    public class SpecialEnemy : Unit, IPoolable<SpecialEnemy.SpawnContext, SpecialEnemyPool>
+    {
+        protected EnemyStats enemyStats => (EnemyStats)Stats;
+
+        private Vector3 direction;
+
+        public void OnDespawned()
+        {
+        }
+
+        public void OnSpawned(SpawnContext context, SpecialEnemyPool pool)
+        {
+        }
+
+        protected override void MovementFunction()
+        {
+            transform.position += direction * enemyStats.FlySpeed * Time.deltaTime;
+        }
+
+        protected override void Shoot()
+        {
+        }
+
+        public struct SpawnContext
+        {
+
+        }
+    }
+}
---
+++ Assets/Code/Gameplay/Units/Enemies/SpecialEnemyPool.cs
@@ -0,0 +1,40 @@
+﻿using Assets.Code.Gameplay.Units.Enemies;
+using System.Collections.Generic;
+using Zenject;
+
+namespace Game.Gameplay
+{
+    public class SpecialEnemyPool : MonoMemoryPool<SpecialEnemy.SpawnContext, SpecialEnemy>
+    {
+        private List<SpecialEnemy> spawnedItems = new List<SpecialEnemy>();
+
+        protected override void Reinitialize(SpecialEnemy.SpawnContext p1, SpecialEnemy item)
+        {
+            item.OnSpawned(p1, this);
+            base.Reinitialize(p1, item);
+        }
+
+        protected override void OnSpawned(SpecialEnemy item)
+        {
+            base.OnSpawned(item);
+            spawnedItems.Add(item);
+        }
+
+        protected override void OnDespawned(SpecialEnemy item)
+        {
+            base.OnDespawned(item);
+            item.OnDespawned();
+            spawnedItems.Remove(item);
+            //ConditionalLogger.Log("enemy despawned");
+        }
+
+        public void DespawnAll()
+        {
+            for (int i = 0; i < spawnedItems.Count;)
+            {
+                this.Despawn(spawnedItems[i]);
+            }
+            spawnedItems.Clear();
+        }
+    }
+}
